ff6e87fb2c0c2d8bfa535fbcf1fb14a0fa3fc70b
isacgw <isac.geisewall@hyperisland.se>
2022-11-20 13:00:01 +0100

    small fix

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import styles from '../styles/Card.module.css';
 
-const Card = ({title, image, url}) => {
+const Card = ({title, image, url, id}) => {
   return (
-    <div className={styles.card}>
+    <div className={styles.card} key={id}>
       <div className={styles.imageContainer}>
       <img src={image} alt="" className={styles.image} />
       </div>
       <div className={styles.content}>
         <h2>{title}</h2>
-        <a href={url} target="_blank">Till produkten</a>
+        <a href={url} target="_blank" rel="noreferrer">Till produkten</a>
       </div>
     </div>
   )
